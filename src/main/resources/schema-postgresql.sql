4a2a2daae98367b7ff2da0fad78fed062df929fc
Marcus Matias <marcusmatias06@gmail.com>
2024-06-03 00:16:55 -0300

    Corrigindo bugs de cadastro de usuário

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Usuario (
      id serial PRIMARY KEY,
-     nome  varchar(50),
-     senha   varchar(11)
+     nome  varchar(50) UNIQUE NOT NULL,
+     senha   varchar(11) NOT NULL
 );
